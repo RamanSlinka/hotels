da9c943a8c34f17b866fe4dc75e525256fc4b83e
RamanSlinka <romuch1986@gmail.com>
2022-03-15 23:08:35 +0100

    created header component and added module.scss (used scss , added react-icons)

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import image from '../../assets/image_hotel.jpg';
+import style from './Header.module.scss';
+import {AiFillStar, AiOutlineStar} from "react-icons/ai";
+
+const Header = () => {
+    return (
+        <header className={style.header}>
+            <div>
+                <img
+                className={style.headImage}
+                src={image} alt="hotel"/>
+                <div className={style.line}> </div>
+            </div>
+            <div className={style.filterBlock}>
+                <div className={style.starsWrapper}>
+                    <span><AiFillStar/></span>
+                    <span><AiFillStar/></span>
+                    <span><AiFillStar/></span>
+                    <span><AiOutlineStar/></span>
+                    <span><AiOutlineStar/></span>
+                    </div>
+                <div className={style.adultWrapper}>
+                    <p>Adults : </p>
+                    <button className={style.button}>+</button>
+                    <p>1</p>
+                    <button className={style.button}>-</button>
+                </div>
+                <div className={style.childrenWrapper}>
+                    <p>Children : </p>
+                    <button className={style.button}>+</button>
+                    <p>1</p>
+                    <button className={style.button}>-</button>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
